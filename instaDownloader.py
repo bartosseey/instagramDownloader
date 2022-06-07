8fab76c05826f5ef0fa2308bd674a68471fd0c83
bartosseey <b.kapusniak11@gmail.com>
2022-06-07 21:26:21 +0200

    todo:logging in and downloading photos

--- instaDownloader.py
+++ instaDownloader.py
@@ -1,16 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
 
 
+def openWeb():
+    #dir to chromedriver
+    s=Service("C:\ChromeDriver\chromedriver.exe")
 
+    #start windows maximized to allow scroll
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    driver = webdriver.Chrome(service=s, options=options)
+    #launch chrome with filmweb open
+    driver.get('https://www.instagram.com/paninaa.d/')
+    time.sleep(10)
+    html = driver.page_source
+    driver.close()
+    return html
 
 
 if __name__=="__main__":
-    url = 'https://www.instagram.com/paninaa.d/'
-    req = requests.get(url)
+    html = openWeb()
 
+    soup = BeautifulSoup(html, 'html.parser')
 
+    result = []
 
-    soup = BeautifulSoup(req.text, 'lxml')
+    images = soup.find_all('img', src="True")
 
-    print(soup.prettify())
+    for i in images:
+        result.append(i['src'])
+
+    print(result)
+
