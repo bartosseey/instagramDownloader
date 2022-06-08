1c30b099907d4a552137592d79e47ffd45313c3d
bartosseey <b.kapusniak11@gmail.com>
2022-06-08 15:49:11 +0200

    download working, idk what about private accounts

--- instaDownloader.py
+++ instaDownloader.py
@@ -7,68 +7,86 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 import time
 import account
+import os
+import wget
 
 
-def openWeb():
-    #dir to chromedriver
+def openWeb(profile):
     s=Service("C:\ChromeDriver\chromedriver.exe")
 
     name = account.username()
     passw = account.password()
 
-
-    #start windows maximized to allow scroll
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     driver = webdriver.Chrome(service=s, options=options)
-    #launch chrome with filmweb open
-    driver.get('https://www.instagram.com/cats_of_instagram/')
+    driver.get(profile)
     
-    driver.find_element(by=By.XPATH, value='/html/body/div[4]/div/div/button[2]').click()
+    driver.find_element(by=By.XPATH, value='/html/body/div[4]/div/div/button[2]')\
+        .click()
 
     time.sleep(2)
 
-    #target username
-    username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+    username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable\
+        ((By.CSS_SELECTOR, "input[name='username']")))
+    password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable\
+        ((By.CSS_SELECTOR, "input[name='password']")))
 
-    #enter username and password
     username.clear()
     username.send_keys(name)
     password.clear()
     password.send_keys(passw)
-
-    #target the login button and click it
-    button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+    button = WebDriverWait(driver, 2).until(EC.element_to_be_clickable\
+        ((By.CSS_SELECTOR, "button[type='submit']"))).click()
     time.sleep(5)
-    alert = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Nie teraz")]'))).click()
+    alert = WebDriverWait(driver, 15).until(EC.element_to_be_clickable\
+            ((By.XPATH, '//button[contains(text(), "Nie teraz")]'))).click()
     #alert2 = WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
     
-
     for j in range(20):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(0.5)
     
-    time.sleep(30)
+    time.sleep(2)
     html = driver.page_source
     driver.close()
     return html
 
 
+def savePhotos(images, folderName):
+    path = os.getcwd()
+    path = os.path.join(path, folderName)
 
-if __name__=="__main__":
-    html = openWeb()
+    os.mkdir(path)
+    counter = 0
+    for image in images:
+        fileName = str(counter) + '.jpg'
+        save_as = os.path.join(path, fileName)
+        wget.download(image, save_as)
+        print("Saved {} in {}.".format(fileName, path))
+        counter += 1
 
+
+def scrap(folderName, profile):
+    html = openWeb(profile)
     soup = BeautifulSoup(html, 'lxml')
-
     result = []
-
     images = soup.find_all('div', {'class':'_aagv'})
 
     for i in images:
         result.append(i.img['src'])
 
-    print(result)
+    savePhotos(result, folderName)
+
+
+if __name__=="__main__":
+    print("-----------Instagram Downloader----------------")
+    print("-----------------------------------------------")
+    folderName = input("Folder name:")
+    profile = input("Profile link:")
+    scrap(folderName, profile)
+    print("-----------------------------------------------")
+
 
